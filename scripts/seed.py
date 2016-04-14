7c381b5842e6c2405bb453080d4ed5822cd1d5d6
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-15 05:01:48 +1000

    Stubbed out script logic

--- scripts/seed.py
+++ scripts/seed.py
@@ -1,1 +1,17 @@
 # Seeds the database from the CSV files in ./data/csv.
+
+# - Remove database
+# - Create database from schema
+
+# - Create 1..5 for impacts
+# - Import all from people.csv
+
+# - For each achievement
+#   - Assign person
+#   - Assign impact
+#   - For each tag:
+#     - Create and assign
+#   - Save
+
+# - For each award
+#   - Create and assign
