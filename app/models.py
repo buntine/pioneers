f4edc4380a197060cd29d578dfd34720a1291a21
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-16 20:05:36 +1000

    Fleshed out Pony models

--- app/models.py
+++ app/models.py
@@ -3,8 +3,41 @@ import os
 
 db = Database()
 
+class Impact(db.Entity):
+    value = Required(int)
+    achievements = Set("Achievement")
+
+class Person(db.Entity):
+    achievements = Set("Achievement")
+    wins = Set("Win")
+    name = Required(str)
+    gender = Required(str)
+    yob = Required(int)
+    yod = Required(int)
+    biography = Required(str)
+    picture = Required(str)
+    source = Required(str)
+
+class Award(db.Entity):
+    wins = Set("Win")
+    name = Required(str)
+
+class Win(db.Entity):
+    people = Set("Person")
+    awards = Set("Award")
+    year = Required(int)
+
 class Tag(db.Entity):
     name = Required(str)
+    achievements = Set("Achievement")
+
+class Achievement(db.Entity):
+    tags = Set("Tag")
+    person = Required(Person)
+    impact = Required(Impact)
+    year = Required(int)
+    description = Required(str)
+    source = Required(str)
 
 db.bind("sqlite", "../db/pioneers.sqlite3", create_db=True)
 db.generate_mapping(create_tables=True)
