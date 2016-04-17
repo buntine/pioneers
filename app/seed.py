b4055fe402c0d956ff333e0170b3b35374d11ca6
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-17 20:00:33 +1000

    Removed direct use of sqlite entirely

--- app/seed.py
+++ app/seed.py
@@ -1,17 +1,14 @@
 #! /usr/bin/env python
 
 import subprocess
-import sqlite3
 import csv
 
 from models import *
 from pony.orm import *
 
 # Seeds the database from the CSV files in ./data/csv.
+# WARNING: This import is destructive. All existing data will be purged!
 
-conn = sqlite3.connect("db/pioneers.sqlite3")
-conn.text_factory = sqlite3.Binary
-db = conn.cursor()
 print "Opened database."
 
 with db_session:
@@ -35,33 +32,25 @@ with open("data/csv/people.csv", "rb") as csvfile:
 with open("data/csv/achievements.csv", "rb") as csvfile:
     achievements = csv.DictReader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True)
 
-    for row in achievements:
-        db.execute("SELECT id FROM people WHERE name = ?", (row["Name"],))
-        person = db.fetchone()
+    with db_session:
+        for row in achievements:
+            person = Person.get(name = row["Name"])
+            impact = Impact.get(value = int(row["Impact"]))
 
-        db.execute("SELECT id FROM impacts WHERE value = ?", (int(row["Impact"]),))
-        impact = db.fetchone()
+            if person and impact:
+                tags = map(lambda t: t.strip(), row["Tags"].split(","))
+                tags = map(lambda t: Tag.get(name = t) or Tag(name = t), tags)
 
-        if person and impact:
-            db.execute("INSERT INTO achievements (person_id, impact_id, year, description, source) VALUES (?, ?, ?, ?, ?)",
-                       (person[0], impact[0], row["Date"], row["Achievement"], row["Source"]))
+                Achievement(person = person, impact = impact, year = row["Date"], description = row["Achievement"], source = row["Source"],
+                            tags = tags)
 
-            conn.commit()
-            print "Created Achievement for %s" % (row["Name"],)
+                commit()
 
-            tags = map(lambda t: t.strip(), row["Tags"].split(","))
+                print "Created Achievement for %s" % (row["Name"],)
+            else:
+                print "WARN: Unknown person or impact (%s, %s). Skipping..." % (row["Name"], row["Impact"])
+                continue
 
-            for t in tags:
-                db.execute("SELECT id FROM tags WHERE name = ?", (t,))
-                tag = db.fetchone()
-
-                if tag:
-                    db.execute
-
-        else:
-            print "WARN: Unknown person or impact (%s, %s). Skipping..." % (row["Name"], row["Impact"])
-            continue
-
-    print "Created Achievements."
+        print "Created Achievements."
  
 # TODO: Create awards here.
