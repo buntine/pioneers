72e6966a698d7f276c680ff892240e387e9a0439
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-17 20:30:29 +1000

    Small import refactor

--- app/seed.py
+++ app/seed.py
@@ -29,9 +29,9 @@ with open("data/csv/people.csv", "rb") as csvfile:
         for row in people:
             Person(name = row["Name"], gender = row["Gender"], country = row["Country"], yob = row["Born"], yod = row["Died"] if len(row["Died"]) > 0 else 0,
                    biography = row["Biography"], picture = row["Picture"], source = row["Source"])
+            print "Created Person: %s" % row["Name"]
 
         commit()
-        print "Created People."
 
 with open("data/csv/achievements.csv", "rb") as csvfile:
     achievements = csv.DictReader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True)
@@ -44,20 +44,18 @@ with open("data/csv/achievements.csv", "rb") as csvfile:
             impact = Impact.get(value = int(row["Impact"]))
 
             if person and impact:
-                tags = map(lambda t: t.strip(), row["Tags"].split(","))
-                tags = map(lambda t: Tag.get(name = t) or Tag(name = t), tags)
+                tags = map(lambda t: Tag.get(name = t) or Tag(name = t),
+                           map(lambda t: t.strip(), row["Tags"].split(",")))
 
                 Achievement(person = person, impact = impact, year = row["Date"], description = row["Achievement"], source = row["Source"],
                             tags = tags)
 
                 commit()
 
-                print "Created Achievement for %s" % (row["Name"],)
+                print "Created Achievement for %s" % person.name
             else:
                 print "WARN: Unknown person or impact (%s, %s). Skipping..." % (row["Name"], row["Impact"])
                 continue
-
-        print "Created Achievements."
 
 with open("data/csv/awards.csv", "rb") as csvfile:
     wins = csv.DictReader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True)
