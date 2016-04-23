2e4644d0a28b5e35c726369f3f18e091fa10d0d0
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-24 01:17:24 +1000

    Added example output for people

--- app/app.py
+++ app/app.py
@@ -12,5 +12,15 @@ def index():
     people = Person.select()
     return str(len(people))
 
+@app.route("/people")
+@db_session
+def people():
+    # Get tags
+    # Get People and mix in achievements for tags
+    # Markup results with "total" which is sum of "importance" of achievements
+    # Example:
+    people = [{"name": "Andrew", "achievements": [{"description": "Invented everything", "year": 1945}], "total": 12}]
+    return people
+
 if __name__ == "__main__":
     app.run(debug=DEBUG)
