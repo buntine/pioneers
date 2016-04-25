055eb5d43ac36b60b75c8d70b655d0490d29ad49
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-26 01:37:53 +1000

    Some simple helpers for generating correct JSON

--- app/app.py
+++ app/app.py
@@ -1,18 +1,29 @@
 from flask import Flask, request, render_template, jsonify
 from models import *
 from pony.orm import *
+from collections import defaultdict
 
 app = Flask(__name__)
 app.config.from_envvar('SETTINGS', silent=True)
 
-def dictify(people):
-    "Collects a list of (person, achievement) into a dict of people->achievements.
-     Also decorates dict with sum of importance of achievements."
-    people
+def expand_achievements(achievements):
+    return map(lambda a: {"description": a.description}, achievements)
+
+def to_list(people):
+    "Translates a dict of people->achievements into a list people."
+    return [{"name": p.name,
+             "achievements": expand_achievements(a)} for p, a in people.iteritems()]
+
+def to_dict(people):
+    "Collects a list of (person, achievement) into a dict of people->achievements."
+    p = defaultdict(list)
+    for k, v in people: p[k].append(v)
+
+    return p
 
 def all_achievements(people):
     "Adds remaining achievements for each person."
-    people
+    return people
 
 @app.route("/")
 @db_session
@@ -29,7 +40,10 @@ def people():
                                for t in a.tags if t.name in tags)
 
     people = all_achievements(
-               to_dict(people[:]))
+               to_list(
+                 to_dict(results[:])))
+
+    print people
 
     return jsonify(people=people)
 
