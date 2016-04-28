451e595e1d1370d5b0b17f3e5e78b830f126e04c
Bunts Thy Unholy <bunts@hhd.com.au>
2016-04-29 04:48:47 +1000

    Experimenting

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,5 +1,33 @@
 declare var $:any;
 
+interface IWin {
+  name: string;
+  year: number;
+}
+
+interface IAchievement {
+  description: string;
+  source: string;
+  impact: number;
+  year: number;
+}
+
+interface IPerson {
+  name: string;
+  country: string;
+  gender: string;
+  impact: number;
+  biography: string;
+  picture: string;
+  source: string;
+  yob: number;
+  yod: number;
+  wins: Array<IWin>;
+  achievements: Array<IAchievement>;
+}
+
 $(function(){
-  alert("Ass");
+  $.getJSON("/people", {"tag": ["www", "internet"], "op": "OR"},
+    (d:string, s:string, x:any) => {console.log(d);}
+  );
 });
