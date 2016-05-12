68775d71ea19335197bb74e0414d6cf36b3fba6b
Bunts Thy Unholy <bunts@hhd.com.au>
2016-05-13 07:57:18 +1000

    Finished op switch

--- typescript/op_switch.ts
+++ typescript/op_switch.ts
@@ -6,7 +6,7 @@ class OpSwitch {
   constructor(public svg:Snap.Paper) {
     this.coords = [
       [2, 15, "or"],
-      [48, 52, "and"]
+      [48, 52, "and"],
     ];
   }
 
@@ -19,15 +19,12 @@ class OpSwitch {
     let op_group = this.svg.group(this.button, this.text);
 
     op_group.click((e:MouseEvent) => {
-      if (this.text.attr("text") == "or") {
-        this.button.animate({x: 48}, 150);
-        this.text.animate({x: 52}, 150);
-        this.text.attr({text: "and"});
-      } else {
-        this.button.animate({x: 2}, 150);
-        this.text.animate({x: 15}, 150);
-        this.text.attr({text: "or"});
-      }
+      let n = (this.text.attr("text") == this.coords[0][2]) ? 1 : 0;
+      let c = this.coords[n];
+
+      this.button.animate({x: c[0]}, 150);
+      this.text.animate({x: c[1]}, 150);
+      this.text.attr({text: c[2]});
 
       f(this.text.attr("text"));
     });
