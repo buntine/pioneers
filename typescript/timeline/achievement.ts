952877b25d1d11f36b35ec37c71f6f4b64d6b308
Bunts Thy Unholy <bunts@hhd.com.au>
2016-06-04 20:21:39 +1000

    Simple halos. Brute forcing this until I get the visual rights

--- typescript/timeline/achievement.ts
+++ typescript/timeline/achievement.ts
@@ -14,10 +14,13 @@ namespace Timeline {
 
         public draw(columnWidth: number, person: Structure.Person, svg: Snap.Paper): void {
             let x = (columnWidth * this.column) + ((columnWidth / 2) - this.RADIUS / 2);
-            let y = (columnWidth * this.row) + ((columnWidth / 2) - this.RADIUS / 2);
+            let y = ((columnWidth / 3) * this.row) + ((columnWidth / 6) - this.RADIUS / 2);
+
             let core = svg.circle(x, y, this.RADIUS);
+            let halo = svg.circle(x, y, this.RADIUS * this.details.impact);
 
             core.attr({fill: "green"});
+            halo.attr({fill: "green", opacity: 0.3});
         }
     }
 }
