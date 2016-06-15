50d83c957b08f34223585f83b2b704922fc2469d
Bunts Thy Unholy <bunts@hhd.com.au>
2016-06-15 21:54:36 +1000

    REmoved hover title in prep for new funtionality

--- typescript/impact/title.ts
+++ typescript/impact/title.ts
@@ -9,19 +9,9 @@ namespace Impact {
     }
 
     export class Title {
-        // WARNING: Here be Dragons! 
-        // I'm not really sure how to position SVG elements correctly when their dimensions
-        // are dynamic so everything here is calculated at runtime. It looks kinda' stupid 
-        // and probably is. Please tell me if you know a better way of doing this...
-
         public state: ShowState;
-        public title: Snap.Element;
 
-        private static WAIT = 800;
-        private static HEIGHT = 60;
-        private static FLAG_WIDTH = Math.ceil(Title.HEIGHT * 1.3)
-        private static PADDING = 20;
-        private static BOTTOM_OFFSET = 30;
+        private static WAIT = 600;
 
         constructor(public person: Person) {
             this.state = ShowState.Unhighlighted;
@@ -43,36 +33,9 @@ namespace Impact {
 
         public show(): void {
             if (this.state == ShowState.Zooming) {
-                this.title.animate({fillOpacity: 1}, 300, mina.linear, () => {
-                    this.state = ShowState.Done;
-                });
+                // drawline()
+                this.state = ShowState.Done;
             }
-        }
-
-        public draw(): void {
-            let p = this.person;
-            let mz = Math.max(p.radius * 2, Person.MAX_ZOOM);
-            let mid = new Vector(mz / 2, mz / 2);
-            let pos = new Vector(p.point.x - mid.x, p.point.y + (mid.y - Title.BOTTOM_OFFSET));
-            let details = p.svg.text(pos.x, pos.y, p.details.name);
-            let flagMask = p.svg.image(p.flagPath(), pos.x, pos.y, Title.FLAG_WIDTH, Title.HEIGHT);
-            let flag = p.svg.rect(pos.x, pos.y, Title.FLAG_WIDTH - 1, Title.HEIGHT, 6);
-
-            flag.attr({fill: flagMask.pattern(pos.x - 6, pos.y, Title.FLAG_WIDTH, Title.HEIGHT)});
-            details.attr({fill: "#232323", fontSize: "18px", fontFamily: "sans-serif, arial"});
-
-            let detailsBBox = details.getBBox();
-            let width = Title.FLAG_WIDTH + (Title.PADDING * 2) + detailsBBox.w;
-            let box = p.svg.rect(pos.x, pos.y, width, Title.HEIGHT, 6);
-
-            // Center in remaining space.
-            details.transform(`translate(${Title.FLAG_WIDTH + Title.PADDING - 2}, ${(Title.HEIGHT / 2) + (detailsBBox.h / 4)})`);
-
-            this.title = p.svg.group(box, details, flag);
-            this.title.attr({fill: "#fff", fillOpacity: 0});
-
-            // Readjust to accommodate dynamic width because of pioneers name.
-            this.title.transform(`translate(${(mz - width) / 2}, 0)`);
         }
 
         public zoom(): void {
@@ -96,7 +59,6 @@ namespace Impact {
                 g.animate({transform: "s1,1"}, 200, mina.linear, () => g.remove());
             };
 
-            this.draw();
             this.state = ShowState.Zooming;
 
             g.attr({cursor: "pointer"});
@@ -122,9 +84,7 @@ namespace Impact {
 
         public finalize(): void {
             if (this.shown()) {
-                this.title.animate({fillOpacity: 0}, 150, mina.linear, () => {
-                    this.title.remove();
-                });
+                // removeLine()
             }
 
             this.state = ShowState.Unhighlighted;
