93824055be0a3a7e096d797b859c7789fefe8785
Bunts Thy Unholy <bunts@hhd.com.au>
2016-06-20 00:29:24 +1000

    Added JS compression, concatenation and mangling to build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 tsc && \
+  uglifyjs --compress --mangle --output app/static/js/app.js -- ./typescript/lib/zepto.1.1.6.min.js typescript/lib/magnific-popup.1.1.0.min.js ./typescript/lib/selectivity-full.2.1.0.min.js ./typescript/lib/snapsvg.0.4.1.min.js ./typescript/lib/mustache.2.2.1.min.js ./typescript/lib/app.js && \
   SETTINGS=app/settings.py python ./app/app.py
