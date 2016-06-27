c6a34790d660f25c90bba5040cee5857b0a39ce7
Bunts Thy Unholy <bunts@hhd.com.au>
2016-06-28 08:33:37 +1000

    Added function for parsing tag syntax out of achievement description

--- typescript/pioneer.ts
+++ typescript/pioneer.ts
@@ -10,6 +10,19 @@ class Pioneer {
             let rendered = Mustache.render(template, {
             person: this.details, 
             years: () => { return `${this.details.yob} - ${this.details.yod || ""}` },
+            parseDescription: () => {
+              return (text: string, render: (s: string) => string) => {
+                let rendered = render(text);
+
+                return rendered.replace(/\#\{(.+?)\}/g, (_: string, t: string) => {
+                  let [title, tag] = t.split("|");
+
+                  if (!tag) { tag = title; }
+
+                  return `<a href="#" data-tag="${tag}">${title}</a>`;
+                });
+              }
+            },
             flag: this.flagPath()});
 
             $.magnificPopup.open({
