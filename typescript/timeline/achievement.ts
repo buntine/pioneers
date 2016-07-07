1f72a5655d37b9f3cfb666f55dde4bb88b6d222f
Bunts Thy Unholy <bunts@hhd.com.au>
2016-07-08 01:32:48 +1000

    Added basics for popper class. needs some bug fixing

--- typescript/timeline/achievement.ts
+++ typescript/timeline/achievement.ts
@@ -19,13 +19,14 @@ namespace Timeline {
         private destinationPoint: Vector;
         private currentPoint: Vector;
         private initialPoint: Vector;
+        private radius: number;
 
         public static ATTRACTION_SPEED = 0.13;
         private static RADIUS_FACTOR = 0.125;
         private static MAX_RADIUS = 15;
         private static HALO_MULTIPLIER = 1.36;
 
-        private static COLOURS: Array<string> = [
+        public static COLOURS: Array<string> = [
             "#313c53",
             "#3f4d69",
             "#1b48a1",
@@ -36,29 +37,29 @@ namespace Timeline {
         constructor(achievement: Structure.Achievement, public svg: Snap.Paper) {
             this.state = ShowState.None;
             this.details = achievement;
-            this.popper = new Timeline.Popper(svg);
+            this.popper = new Popper(svg);
             this.row = 0;
             this.column = 0;
         }
 
         public drawHalo(columnSize: number): void {
-            let radius = this.radius(columnSize);
+            this.radius = Math.min(Achievement.MAX_RADIUS, columnSize * Achievement.RADIUS_FACTOR);
+            this.destinationPoint = this.coords(columnSize);
 
-            this.destinationPoint = this.coords(columnSize, radius);
-
-            this.halo = this.svg.circle(this.destinationPoint.x, this.destinationPoint.y, radius);
+            this.halo = this.svg.circle(this.destinationPoint.x, this.destinationPoint.y, this.radius);
             this.halo.attr({fill: "none", borderWidth: 1, stroke: Achievement.COLOURS[1], opacity: 0});
         }
 
         public drawCore(columnSize: number, p: Timeline.Person): void {
             let [w, h] = ["width", "height"].map(a => parseInt(this.svg.attr(a)));
-            let radius = this.radius(columnSize);
 
             this.currentPoint = Vector.randomized(new Vector(0, 0), new Vector(w, h));
             this.initialPoint = this.currentPoint.clone();
 
-            this.core = this.svg.circle(this.initialPoint.x, this.initialPoint.y, radius);
+            this.core = this.svg.circle(this.initialPoint.x, this.initialPoint.y, this.radius);
             this.core.attr({fill: this.fill(), cursor: "pointer"});
+
+            this.popper.draw(this.destinationPoint, this.radius);
 
             this.core.hover((_: MouseEvent) => {
                 let r = parseInt(this.halo.attr("r"));
@@ -68,7 +69,7 @@ namespace Timeline {
                 // If user lets animation complete, continue to show full achievement details.
                 this.core.animate({r: r}, 700, mina.easeout, () => {
                     this.state = ShowState.Shown;
-                    this.popper.expand(p, this.details, r, () => this.unfocus());
+                    this.popper.expand(p, this.details, () => this.unfocus());
                 });
             },
             (_: MouseEvent) => {
@@ -90,8 +91,7 @@ namespace Timeline {
         }
 
         public snap(): void {
-            let radius = parseInt(this.halo.attr("r"));
-            let haloRadius = radius * (this.details.impact * Achievement.HALO_MULTIPLIER);
+            let haloRadius = this.radius * (this.details.impact * Achievement.HALO_MULTIPLIER);
 
             // Snap to exact position.
             this.position(1);
@@ -100,22 +100,18 @@ namespace Timeline {
 
         private unfocus(): void {
             this.state = ShowState.None;
-            this.core.stop().animate({r: radius}, 300, mina.easein);
+            this.core.stop().animate({r: this.radius}, 300, mina.easein);
         }
 
         private fill(): string {
             return Achievement.COLOURS[this.details.impact - 1];
         }
 
-        private coords(columnSize: number, radius: number): Vector {
-            let c = (a: number, max = 99999) => Helpers.centerize(Math.min(max, columnSize), radius / 4, a);
+        private coords(columnSize: number): Vector {
+            let c = (a: number, max = 99999) => Helpers.centerize(Math.min(max, columnSize), this.radius / 4, a);
 
             return new Vector(c(this.column),
                               c(this.row, Timeline.MAX_ROW_SIZE) + Timeline.TOP_PADDING);
-        }
-
-        private radius(columnSize: number) {
-            return Math.min(Achievement.MAX_RADIUS, columnSize * Achievement.RADIUS_FACTOR);
         }
     }
 }
--- typescript/timeline/popper.ts
+++ typescript/timeline/popper.ts
@@ -1,44 +1,61 @@
 namespace Timeline {
     export class Popper {
+        private rect: Snap.Element;
+        private point: Vector;
+        private radius: number;
+
         constructor(public svg: Snap.Paper) { }
 
-        public expand(p: Timeline.Person, a: Structure.Achievement, r: number, onexit: () => void) {
-            this.pop = this.svg.rect(this.currentPoint.x - r, this.currentPoint.y - r, r * 2, r * 2, r, r);
-            this.pop.attr({fill: "#2b2b2b", borderWidth: "1px", stroke: Achievement.COLOURS[1]});
-            this.pop.animate({rx: 3, ry: 3}, 220, mina.easein, () => {
-                this.pop.animate({height: 400, width: 300, x: this.currentPoint.x - 150}, 220, mina.easeout, () => {
-                    this.show(p, r);
+        // TODO: Use one rect and just show/hide it on "expand".
 
-                    let f = (e: MouseEvent) => {
-                        $("#achievement_overlay").hide();
+        public draw(point: Vector, r: number): void {
+            this.point = point;
+            this.radius = r;
+            this.rect = this.svg.rect(point.x - r, point.y - r, r * 2, r * 2, r, r);
+            this.rect.attr({fill: "#2b2b2b",
+                            borderWidth: "1px",
+                            stroke: Achievement.COLOURS[1],
+                            display: "none"});
+        }
 
-                        pop.animate({height: r * 2, width: r * 2, x: this.currentPoint.x - r}, 220, mina.easein, () => {
-                            pop.animate({rx: r, ry: r}, 220, mina.easeout, () => {
-                                pop.remove();
-                                this.state = ShowState.None;
-                                this.core.stop().animate({r: radius}, 300, mina.easein);
+        public expand(p: Timeline.Person, a: Structure.Achievement, onexit: () => void) {
+            let r = this.radius;
+
+            this.rect
+                .attr({display: ""})
+                .animate({rx: 3, ry: 3}, 220, mina.easein, () => {
+                    this.rect.animate({height: 400, width: 300, x: this.point.x - 150}, 220, mina.easeout, () => {
+                        this.show(p, a);
+ 
+                        let f = (e: MouseEvent) => {
+                            $("#achievement_overlay").hide();
+    
+                            this.rect.animate({height: r * 2, width: r * 2, x: this.point.x - r}, 220, mina.easein, () => {
+                                this.rect.animate({rx: r, ry: r}, 220, mina.easeout, () => {
+                                    this.rect.attr({display: "none"});
+                                    onexit();
+                                });
                             });
-                        });
-
-                        this.svg.unmouseover(f);
-                    };
-
-                    this.svg.mouseover(f);
-                });
+    
+                            this.svg.unmouseover(f);
+                        };
+ 
+                        this.svg.mouseover(f);
+                    });
             });
         }
 
-        public show(p: Timeline.Person, r: number): void {
+        public show(p: Timeline.Person, a: Structure.Achievement): void {
             $.get("/static/templates/achievement.mst", (template: string) => {
                 let rendered = Mustache.render(template, {
                     person: p.details, 
-                    achievement: this.details, 
+                    achievement: a, 
                     parseDescription: Helpers.parseDescription,
                 });
 
                 $("#achievement_overlay")
                     .html(rendered)
-                    .css({top: this.destinationPoint.y + 60 - r, left: this.destinationPoint.x - 150})
+                    .css({top: this.point.y + 60 - this.radius, left: this.point.x - 150})
                     .show()
                     .find(".all_achievements")
                     .click((e: MouseEvent) => {
