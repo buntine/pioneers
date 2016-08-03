daa7b25a66f24a5c557ebc1e78abccbd6d162261
Bunts Thy Unholy <bunts@hhd.com.au>
2016-08-03 19:07:47 +1000

    Setting up for initial deployment

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 tsc && \
   uglifyjs --compress --mangle --output app/static/js/app.js -- ./typescript/lib/zepto.1.1.6.min.js typescript/lib/magnific-popup.1.1.0.min.js ./typescript/lib/selectivity-full.2.1.0.min.js ./typescript/lib/snapsvg.0.4.1.min.js ./typescript/lib/mustache.2.2.1.min.js ./typescript/lib/app.js && \
   rm -f app/static/css/app.css && \
-  uglifycss app/static/css/*.css > app/static/css/app.css && \
-  SETTINGS=app/settings.py python ./app/app.py
+  uglifycss app/static/css/*.css > app/static/css/app.css
+
+[ -z "$1" ] && SETTINGS=app/settings.py python ./app/app.py
