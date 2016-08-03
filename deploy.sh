17de6d7b593ce456b3f28b209b1c0e8ee870b796
Bunts Thy Unholy <bunts@hhd.com.au>
2016-08-03 22:29:32 +1000

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+ssh perunwit@computerpionee.rs "\
+  cd $PIONEERSPATH;\
+  echo 'UPDATING SOURCE';\
+  git pull origin master;\
+  echo 'BUILDING TYPESCRIPT';\
+  ./build.sh RUN=0;\
+  echo 'RESTARTING APP SERVER';\
+  sudo systemctl restart pioneers.service;\
+  echo 'DONE'"
