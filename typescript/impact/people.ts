7a4c0c77a5edd86759af845ecc34ce731ce7cacf
Andrew Buntine <info@bunts.io>
2017-06-07 22:37:21 +1000

    Refactored People class now that it's not subclassing Array

--- typescript/impact/people.ts
+++ typescript/impact/people.ts
@@ -6,7 +6,7 @@ namespace Impact {
         private center: Vector;
         public alive: boolean;
         public resolution: Structure.Resolution;
-        public people: Array<Person>;
+        public collection: Array<Person>;
 
         private static MIN_REFINEMENT = 50;
         private static MAX_DELTA = 0.7;
@@ -17,7 +17,7 @@ namespace Impact {
         private static MAX_FRAME_DIFF = 75; // Miliseconds.
 
         constructor() {
-            this.people = [];
+            this.collection = [];
             this.resolution = "High";
             this.totalImpact = 0;
             this.alive = true;
@@ -29,7 +29,11 @@ namespace Impact {
 
         public push(p: Person): number {
             this.totalImpact += p.details.impact;
-            return this.people.push(p);
+            return this.collection.push(p);
+        }
+
+        public forEach(f: (p: Person) => void): void {
+            this.collection.forEach(f);
         }
 
         public pack(callback: () => void): void {
@@ -38,7 +42,7 @@ namespace Impact {
 
             this.alive = true;
 
-            for (let p of this.people) {
+            for (let p of this.collection) {
                 p.draw(unit);
             }
 
@@ -48,7 +52,7 @@ namespace Impact {
         public reset(): void {
             this.alive = false;
 
-            for (let p of this.people) {
+            for (let p of this.collection) {
                 p.reset();
             }
         }
@@ -56,36 +60,36 @@ namespace Impact {
         public clear(): void {
             this.alive = false;
             this.totalImpact = 0;
-            this.people.length = 0;
+            this.collection.length = 0;
         }
 
         private position(ts: number, callback: () => void, iteration = 1): void {
             if (!this.alive) { return; }
 
             let redraw: boolean;
-            let iterations = Math.max(People.MIN_REFINEMENT, this.people.length * People.REFINEMENT_DELTA);
+            let iterations = Math.max(People.MIN_REFINEMENT, this.collection.length * People.REFINEMENT_DELTA);
 
             if (this.resolution == "High") {
-                redraw = iteration % Math.ceil(this.people.length / People.REDRAW_THRESHOLD) == 0;
+                redraw = iteration % Math.ceil(this.collection.length / People.REDRAW_THRESHOLD) == 0;
             } else {
                 redraw = iteration == iterations;
             }
 
             // Sort from closest->furthest to center point.
-            this.people.sort((a: Person, b: Person) => {
+            this.collection.sort((a: Person, b: Person) => {
                 let c = this.center;
                 return a.point.distanceFrom(c) - b.point.distanceFrom(c);
             });
 
             // Detract other particles.
-            for (let i=0;i<this.people.length;i++) {
-                for (let n=i+1;n<this.people.length;n++) {
-                    this.people[i].detract(this.people[n]);
+            for (let i=0;i<this.collection.length;i++) {
+                for (let n=i+1;n<this.collection.length;n++) {
+                    this.collection[i].detract(this.collection[n]);
                 }
             }
 
             // Attract to center point.
-            for (let p of this.people) {
+            for (let p of this.collection) {
                 p.attract(this.center, People.DAMPING_FACTOR / iteration);
 
                 if (redraw) {
@@ -105,7 +109,7 @@ namespace Impact {
                 });
             // Reposition people who've been pushed too far off-screen.
             } else {
-                let pToMove = this.people.filter((p) => p.offScreen());
+                let pToMove = this.collection.filter((p) => p.offScreen());
                 let offset = 0;
 
                 for (let p of pToMove) {
@@ -121,14 +125,14 @@ namespace Impact {
         }
 
         public closestToCenter(): Person {
-            return this.people[0];
+            return this.collection[0];
         }
 
         private findClosestTo(person: Person): Person {
-            let closest = this.people[0],
+            let closest = this.collection[0],
                 point = person.point;
 
-            for (let p of this.people) {
+            for (let p of this.collection) {
                // We only want to find the closest person that is collidable on the Y axis.
                if (person.collidingOn(p, Axis.Y) &&
                    person.details.id != p.details.id &&
@@ -141,7 +145,8 @@ namespace Impact {
         }
  
         private delta(): number {
-            return (this.people.length == 1) ? People.MAX_DELTA : 1 + ((this.people.length - 1) * People.SIZING_DELTA);
+            let l = this.collection.length;
+            return (l == 1) ? People.MAX_DELTA : 1 + ((l - 1) * People.SIZING_DELTA);
         }
     }
 }
